85b7b0e2f4133bab7ced67abb6a5a4833d838806
YasorI13 <s_oroin@hotmail.com>
2022-06-28 15:25:09 +0700

    New Template
    เพิ่ม Template ใหม่เข้ามา

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
-const express = require('express');
-const chalk = require('chalk')
-const debug = require('debug')('app');
-const morgan = require('morgan');
+const express = require('express'); // import express
+const chalk = require('chalk'); // for color output
+const debug = require('debug')('app'); // debug is a function
+const morgan = require('morgan'); // middleware for logging requests
+const path = require('path'); // path is a node module
 
-const app = express();
-const port = 3000;
+const app = express();  // create an express app
+const port = 3000;  // port to listen on
 
-app.use(morgan('combined'));
+app.use(morgan('combined'));    // use morgan to log requests
+app.use(express.static(path.join(__dirname, '/public/'))); // use express to serve static files
 
 app.get("/", (req, res) =>{
     res.send("Hello World!");
